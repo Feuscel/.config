183e04e4bf3a8fd31c95af6926bc3cded2779bca
Feuscel <feuscel@feuscel.dev>
2025-01-12 17:18:36 +0100

    add alias to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,9 +6,65 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #    # smth smth
 #end
 
+#alias 
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias df='df -h'
+alias free='free -m'
+alias more='less'
+
+#Pacman stuff
+alias sps='sudo pacman -S'
+alias spr='sudo pacman -R'
+alias sprs='sudo pacman -Rs'
+alias sprdd='sudo pacman -Rdd'
+alias spqo='sudo pacman -Qo'
+alias spsii='sudo pacman -Sii'
+
+#Fix typo
+alias cd..='cd ..'
+alias pdw='pwd'
+alias udpate='sudo pacman -Syyu'
+alias upate='sudo pacman -Syyu'
+alias updte='sudo pacman -Syyu'
+alias updqte='sudo pacman -Syyu'
+alias upqll='paru -Syu --noconfirm'
+alias upal='paru -Syu --noconfirm'
+
+# Changing "ls" to "exa"
+alias ls='exa -al --color=always --group-directories-first --icons' # my preferred listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.='exa -a | egrep "^\."'
+
+## Colorize the grep command output for ease of use (good for log files)##
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+#continue download
+alias wget="wget -c"
+# Aliases for software managment
+# pacman or pm
+alias pacman='sudo pacman --color auto'
+alias update='sudo pacman -Syyu'
+alias upd='sudo pacman -Syyu'
+
+#ps
+alias psa="ps auxf"
+alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+# add new fonts
+alias update-fc='sudo fc-cache -fv'
+
+
+# add to path 
 fish_add_path /home/feuscel/.spicetify
 fish_add_path /home/feuscel/.cargo/bin
 fish_add_path /opt/nvim-linux64/bin
+
+# Config utilities
 zoxide init --cmd cd fish | source
 atuin init fish | source
 
