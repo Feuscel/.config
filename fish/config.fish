173e8611aa2b3d4573bd1d479062c529f817d16d
Feuscel <feuscel@feuscel.dev>
2025-01-12 17:03:36 +0100

    remove useless config file

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,9 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #end
 
 fish_add_path /home/feuscel/.spicetify
+fish_add_path /home/feuscel/.cargo/bin
+fish_add_path /opt/nvim-linux64/bin
+zoxide init --cmd cd fish | source
+atuin init fish | source
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
---
+++ fish/functions/chpr.fish
@@ -0,0 +1,3 @@
+function chpr --description "Change hyprland's config"
+    micro ~/.config/hypr/hyprland.conf
+end
